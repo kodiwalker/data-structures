17e76490c707bb80d03fe90f1c5426b136fa3282
Kodi <kodi.w13@gmail.com>
2023-03-29 19:08:59 -0600

    Implemented functional and functional with shared methods of stack.js

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,60 @@
-var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Stack = function () {
+  var someInstance = {
+    storage: {}
+  };
+
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
 
+  push: function (value) {
+    // if storage is empty 
+    if (Object.keys(this.storage).length === 0) {
+      this.storage['1'] = value;
+    } else {
+      var keys = Object.keys(this.storage);
+      var numberKeys = [];
+      keys.forEach(function (key) {
+        numberKeys.push(Number(key));
+      });
+      var max = Math.max.apply(null, numberKeys);
+      var newKey = max + 1;
+      this.storage[newKey] = value;
+    }
+    // set key to 1 and value as property value
+    // else
+  },
+  pop: function () {
+    // if storage is empty, return undefined
+    // else find highest key num, delete it, and return the value.
+    if (Object.keys(this.storage).length === 0) {
+      return undefined;
+    } else {
+      var keys = Object.keys(this.storage);
+      var numberKeys = [];
+      keys.forEach(function (key) {
+        numberKeys.push(Number(key));
+      });
+      var max = Math.max.apply(null, numberKeys);
+      var result = this.storage[max];
+      delete this.storage[max];
+      return result;
+    }
+  },
+  size: function () {
+    // find highest key num and return it
+    if (Object.keys(this.storage).length === 0) {
+      return 0;
+    } else {
+      var keys = Object.keys(this.storage);
+      var numberKeys = [];
+      keys.forEach(function (key) {
+        numberKeys.push(Number(key));
+      });
+      return Math.max.apply(null, numberKeys);
+    }
+  }
+};
 
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,66 @@
-var Stack = function() {
+var Stack = function () {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
-  someInstance.push = function(value) {
+  someInstance.push = function (value) {
+    // variable that equals array of object keys
+    // variable that stores integered object keys
+    // iterate over array of object keys
+    // make each an integer with number() and push to array
+    // find highest number of new array with math.max()
+    if (Object.keys(storage).length === 0) {
+      storage['1'] = value;
+    } else {
+      var keys = Object.keys(storage);
+      var numberKeys = [];
+      keys.forEach(function (key) {
+        numberKeys.push(Number(key));
+      });
+      var max = Math.max.apply(null, numberKeys);
+      var newKey = max + 1;
+      storage[newKey] = value;
+    }
+
+    // push value to top of stack with highest number + 1 as key
   };
 
-  someInstance.pop = function() {
+  someInstance.pop = function () {
+    // find highest key value in storage
+
+
+    if (Object.keys(storage).length === 0) {
+      return undefined;
+    } else {
+      var keys = Object.keys(storage);
+      var numberKeys = [];
+      keys.forEach(function (key) {
+        numberKeys.push(Number(key));
+      });
+      var max = Math.max.apply(null, numberKeys);
+      var result = storage[max];
+      delete storage[max];
+      return result;
+    }
+    // pop it
+    // return it
   };
 
-  someInstance.size = function() {
+  someInstance.size = function () {
+    // find highest key number
+    if (Object.keys(storage).length === 0) {
+      return 0;
+    } else {
+      var keys = Object.keys(storage);
+      var numberKeys = [];
+      keys.forEach(function (key) {
+        numberKeys.push(Number(key));
+      });
+      return Math.max.apply(null, numberKeys);
+    }
+    // return the number
   };
 
   return someInstance;
