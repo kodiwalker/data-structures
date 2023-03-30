4749e833dd2ef1bb82e3e27c28af20892f50248a
Kodi <kodi.w13@gmail.com>
2023-03-30 13:33:48 -0600

    Implemented pseudoclassical and es6 stack.js with passing tests

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,31 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.amount = 0;
+  }
+
+  push(value) {
+    if (this.amount === 0) {
+      this.amount += 1;
+      this.storage[this.amount] = value;
+    } else {
+      this.amount += 1;
+      this.storage[this.amount] = value;
+    }
+  }
+
+  pop() {
+    var top = this.storage[this.amount];
+    delete this.storage[this.amount];
+    if (this.amount !== 0) {
+      this.amount -= 1;
+    }
+    return top;
+  }
+
+  size() {
+    return this.amount;
   }
 
 }
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,29 @@
-var Stack = function() {
+var Stack = function () {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.amount = 0;
 };
 
+Stack.prototype.push = function (value) {
+  if (this.amount === 0) {
+    this.storage['1'] = value;
+    this.amount += 1;
+  } else {
+    this.storage[this.amount + 1] = value;
+    this.amount += 1;
+  }
+};
 
+Stack.prototype.pop = function () {
+  var top = this.storage[this.amount];
+  delete this.storage[this.amount];
+  if (this.amount !== 0) {
+    this.amount -= 1;
+  }
+  return top;
+};
+
+Stack.prototype.size = function () {
+  return this.amount;
+};
